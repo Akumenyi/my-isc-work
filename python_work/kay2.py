397d1a2ac90b12d8fde247ac941f0946849ed003
User 01 <user01@unst03.(none)>
2017-11-15 13:42:45 +0000

    Second day Python

---
+++ python_work/kay2.py
@@ -0,0 +1,112 @@
+name = "Kwesi"
+
+#recap working with the "if, elif and else"
+if name == "Temi":
+    print "Hi Temi"
+elif name == "Kwesi":
+    print "Hello"
+else:
+    print "Who are you?" 
+
+
+#excercise on tuples
+t = (1,)
+print t[0]
+x = range(100, 201)
+print x
+tom = tuple(x)
+print tom[0], tom[-1]
+
+
+#using enumerate
+mylist = [23, "hi", 2.4e-10]
+for (count,item) in enumerate(mylist):
+    print count,item 
+
+
+#unpack list to tuple in a line
+(first,middle,last) = mylist
+print "values are", first, middle, last
+first,middle,last = middle,last,first
+print first, middle, last
+print 'Done!'
+
+
+#read write manipulate data
+with open('weather.csv', 'r') as reader:
+    data = reader.read()
+print data
+
+
+with open('weather.csv', 'r') as reader:
+    line = reader.readline()
+    while line:
+        line = reader.readline()
+        print line
+    print "It's over"
+
+with open('weather.csv', 'r') as reader:
+    rf = reader.readline()		#read headers
+    print 'rf reads headers:', rf
+    rain = []
+    for line in reader.readlines():
+        r =float(line.strip().split(",")[-1])
+        rain.append(r)
+print rain
+with open('myrainfall.txt','w') as writer: #write rain output to text str
+    for r in rain:
+        writer.write(str(r) + '\n')
+
+#advanced data manipulation
+import struct 
+bin_data = struct.pack("bbbb", 123, 12, 45, 34) #pack into 4 bytes
+with open('mybinary.dat', 'wb') as bwriter:     #using with, wrt to file
+    bwriter.write(bin_data)                     #called bin_data
+with open('mybinary.dat', 'rb') as breader:
+    bin_data2 = breader.read()
+data = struct.unpack("bbbb", bin_data2)		#unpack into bin_data2
+print 'Data is:', data				#print data
+
+
+#dealing with strings
+s = "I love to write python"
+for a in s:
+    print a
+b = s[4]
+print b
+c = s[-1]
+print c
+print 'length is ',len(s)
+print s[0], s[0][0], s[0][0][0]
+
+s = "I love to write python"
+split_s = s.split()
+for word in split_s:
+    if word.find("i") > -1:
+        print "I found 'i' in: '{0}'".format(word)    
+
+something = "Completely Different"
+print 'printing directory...',dir(something)
+print something.count('t')
+print something.find('plete')
+print something.split('e')
+thing2 = something.replace('Different','Silly')
+print thing2
+#something[0] = 'B' #gives error cos strings are immuatable!!
+
+#using aliases
+a = [0, 1, 2]
+b = a
+print a, b
+b[0] = "hello"
+print a, b
+a.append(3)
+print a, b
+
+a = "can I change"
+b = a
+print a, b
+b = 'different'
+print a, b
+
+
